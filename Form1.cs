07264cf01fcf5ebf00cd2e176101c18203996431
lerufino <121639527+lerufino@users.noreply.github.com>
2023-08-21 17:02:05 -0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace AjudanteDBA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AjudanteDBA
+ Ferramenta para auxiliar no trabalho do DB Admin na gestão de uma instância SQL
